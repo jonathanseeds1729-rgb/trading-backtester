738e73abb31162310ab804492e9f763c52cd32a0
Jonathan Seeds <jonathanseeds1729@gmail.com>
2026-08-01 22:17:29 +0100

    Add matplotlib chart of AAPL closing price

--- fetch_data.py
+++ fetch_data.py
@@ -1,4 +1,8 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
 
 data = yf.download("AAPL", start="2020-01-01", end="2024-01-01")
-print(data.head())
+data["Close"].plot(title = "AAPL Closing Price")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
